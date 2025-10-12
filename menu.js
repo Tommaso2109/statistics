0aceaea80e313488bbd7133796dc1a0552d0e48d
unknown <rinalditommaso23@gmail.com>
2025-10-12 21:58:29 +0200

    pdf sistemato

--- menu.js
+++ menu.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var menuBtn = document.getElementById('menuBtn');
     var dropdownMenu = document.getElementById('dropdownMenu');
 
-    menuBtn.onclick = function(event) {
-        event.stopPropagation();
-        dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
-    };
+    if (menuBtn && dropdownMenu) {
+        menuBtn.onclick = function(event) {
+            event.stopPropagation();
+            dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
+        };
 
-    document.addEventListener('click', function(event) {
-        if (!dropdownMenu.contains(event.target) && event.target !== menuBtn) {
-            dropdownMenu.style.display = 'none';
-        }
-    });
+        document.addEventListener('click', function(event) {
+            if (!dropdownMenu.contains(event.target) && event.target !== menuBtn) {
+                dropdownMenu.style.display = 'none';
+            }
+        });
+    }
 });
 
